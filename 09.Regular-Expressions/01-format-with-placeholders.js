ac85a74fda86077deef7f6dbcf0a64ce29535499
bozhidar.slavov <bozhidar.slavov96@gmail.com>
2016-07-25 16:12:36 +0300

    Regex homework tasks!

---
+++ 09.Regular-Expressions/01-format-with-placeholders.js
@@ -0,0 +1,18 @@
+﻿// Problem 1
+// Write a function that formats a string. The function should have placeholders, as shown in the example
+// Add the function to the String.prototype
+
+function solve(args) {
+    String.prototype.format = function (options) {
+        var prop,
+            result = this;
+
+        for (prop in options) {
+            result = result.replace(new RegExp('#{' + prop + '}', 'g'), options[prop]);
+        }
+
+        return result;
+    };
+
+    console.log(args[1].format(JSON.parse(args[0])));
+}
---
+++ 09.Regular-Expressions/02-html-binding.js
@@ -0,0 +1,38 @@
+﻿// Problem 2
+// Write a function that puts the value of an object into the content/attributes of HTML tags.
+// Add the function to the String.prototype
+
+function solve(args) {
+    var options = JSON.parse(args[0]);
+    var inputStr = args[1].replace(/'/, '"');
+
+    String.prototype.bind = function (parameters) {
+        var result = this;
+        var currentMatch;
+
+        regExAttr = new RegExp('data-bind-(.*?)="(.*?)"', 'gmi');
+
+        while (currentMatch = regExAttr.exec(result)) {
+            var arr;
+            var index = result.indexOf('>');
+
+            if (result[index - 1] === '/') {
+                index--;
+            }
+
+            if (currentMatch[1] !== 'content') {
+                arr = result.split('');
+                arr.splice(index, 0, " " + currentMatch[1] + '="' + parameters[currentMatch[2]] + '"');
+                result = arr.join('');
+            } else {
+                arr = result.split('');
+                arr.splice(index + 1, 0, parameters[currentMatch[2]]);
+                result = arr.join('');
+            }
+        }
+
+        return result;
+    };
+
+    return inputStr.bind(options);
+}
